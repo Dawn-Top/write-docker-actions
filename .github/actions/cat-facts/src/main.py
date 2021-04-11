237c8dc85b925114dbff4a992a1c415864d78174
Dawn-Top <81514318+Dawn-Top@users.noreply.github.com>
2021-04-11 20:29:29 +0100

    Update main.py

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -17,10 +17,7 @@ for fact in r_obj_list:
 
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
-def select_random_fact(fact_arr):
-    return fact_arr[random.randint(0, len(fact_list)+1)]
-
-random_fact = select_random_fact(fact_list)
+random_fact = fact_arr[random.randint(0, len(fact_list))]
 
 # Print the individual randomly returned cat-fact
 print(random_fact)
